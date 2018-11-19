56e863d01d817f6ea835ea72e6e486baac5363b2
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2018-11-18 23:08:28 -0500

    fixed allowed_hosts

--- latest/rango/settings.py
+++ latest/rango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zm^g(@5!cthaa()m79h5ubtw3bqzl50-h^_84*4uww6an^vjhb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rango.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rangodjango.herokuapp.com']
 
 
 # Application definition
