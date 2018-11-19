ac4c6c844f3c8956e1f0e9cbfc2efead7b4218db
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2018-11-19 02:34:08 -0500

    added db updates

--- latest/run.sh
+++ latest/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # this file tells the server how to run Django, and we use waitress for this example, at least to deploy to Heroku
+python3 latest/manage.py makemigrations
+python3 latest/manage.py migrate
 python3 latest/manage.py serve 0.0.0.0 $PORT
