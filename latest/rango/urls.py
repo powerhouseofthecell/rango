64f3df32ea721ca74e4a9c56f5fadf665c3d2df3
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2018-11-18 23:50:18 -0500

    added path to web urls

--- latest/rango/urls.py
+++ latest/rango/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+
+import rango.web.urls as web_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    re_path('', include(web_urls)),
+]
 
