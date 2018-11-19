64aaa260aafade3b4c6bd75035959b3e850c7b9a
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2018-11-19 01:12:55 -0500

    attempted stage 04

--- latest/web/views.py
+++ latest/web/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+import requests as r
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def posts(request):
     context = dict()
-    context['post_content'] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et tamen tantis vectigalibus ad liberalitatem utens etiam sine hac Pyladea amicitia multorum te benivolentia praeclare tuebere et munies. Ita redarguitur ipse a sese, convincunturque scripta eius probitate ipsius ac moribus. Ratio ista, quam defendis, praecepta, quae didicisti, quae probas, funditus evertunt amicitiam, quamvis eam Epicurus, ut facit, in caelum efferat laudibus. Ut ad minora veniam, mathematici, poëtae, musici, medici denique ex hac tamquam omnium artificum officina profecti sunt. Scientiam pollicentur, quam non erat mirum sapientiae cupido patria esse cariorem.'
+
+    # use this super friendly API to grab 10 lorem ipsums
+    resp = r.get('https://loripsum.net/api/10/long/plaintext')
+
+    # split those into their respective parts
+    context['posts'] = [el.strip() for el in resp.text.splitlines() if el]
+
     return render(request, 'posts.html', context=context)
